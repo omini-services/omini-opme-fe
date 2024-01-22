c93758d12797b9a5b4fa9e47f3e19903798b5971
Danilo Kasparian <dan.acceto@gmail.com>
2024-01-22 18:16:29 -0500

    feat: remove error file

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,69 +1,69 @@
-import { User, onAuthStateChanged } from 'firebase/auth';
-import {
-  createContext,
-  useCallback,
-  useContext,
-  useEffect,
-  useState,
-} from 'react';
+// import { User, onAuthStateChanged } from 'firebase/auth';
+// import {
+//   createContext,
+//   useCallback,
+//   useContext,
+//   useEffect,
+//   useState,
+// } from 'react';
 
-import { authenticationService } from '../../domain/services/authenticationService';
-import { auth } from '../../infrastructure/common/services/firebase';
-import { LoadingScreen } from '../components/LoadingScreen';
+// import { authenticationService } from '../../domain/services/authenticationService';
+// import { auth } from '../../infrastructure/common/services/firebase';
+// import { LoadingScreen } from '../components/LoadingScreen';
 
-interface AuthContextValue {
-  signedIn: boolean;
-  user: User | null;
-  isLoading: boolean;
-  signout(): void;
-}
+// interface AuthContextValue {
+//   signedIn: boolean;
+//   user: User | null;
+//   isLoading: boolean;
+//   signout(): void;
+// }
 
-export const AuthContext = createContext({} as AuthContextValue);
+// export const AuthContext = createContext({} as AuthContextValue);
 
-export function AuthProvider({ children }: { children: React.ReactNode }) {
-  const [signedIn, setSignedIn] = useState<boolean>(false);
-  const [user, setUser] = useState<User | null>(null);
-  const [isLoading, setIsLoading] = useState<boolean>(true);
+// export function AuthProvider({ children }: { children: React.ReactNode }) {
+//   const [signedIn, setSignedIn] = useState<boolean>(false);
+//   const [user, setUser] = useState<User | null>(null);
+//   const [isLoading, setIsLoading] = useState<boolean>(true);
 
-  const signout = useCallback(() => {
-    authenticationService.signout();
+//   const signout = useCallback(() => {
+//     authenticationService.signout();
 
-    setSignedIn(false);
-  }, [auth]);
+//     setSignedIn(false);
+//   }, [auth]);
 
-  useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
-      if (currentUser) {
-        setUser(currentUser);
-        setSignedIn(true);
-      } else {
-        setUser(null);
-        setSignedIn(false);
-      }
+//   useEffect(() => {
+//     const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+//       if (currentUser) {
+//         setUser(currentUser);
+//         setSignedIn(true);
+//       } else {
+//         setUser(null);
+//         setSignedIn(false);
+//       }
 
-      setIsLoading(false);
-    });
+//       setIsLoading(false);
+//     });
 
-    return () => {
-      unsubscribe();
-    };
-  }, []);
+//     return () => {
+//       unsubscribe();
+//     };
+//   }, []);
 
-  return (
-    <AuthContext.Provider
-      value={{
-        // signedIn: isSuccess && signedIn,
-        signedIn,
-        user,
-        isLoading,
-        signout,
-      }}
-    >
-      {isLoading && <LoadingScreen />}
-      {/* {!isFetching && children} */}
-      {!isLoading && children}
-    </AuthContext.Provider>
-  );
-}
+//   return (
+//     <AuthContext.Provider
+//       value={{
+//         // signedIn: isSuccess && signedIn,
+//         signedIn,
+//         user,
+//         isLoading,
+//         signout,
+//       }}
+//     >
+//       {isLoading && <LoadingScreen />}
+//       {/* {!isFetching && children} */}
+//       {!isLoading && children}
+//     </AuthContext.Provider>
+//   );
+// }
 
-export const useAuth = () => useContext(AuthContext);
+// export const useAuth = () => useContext(AuthContext);
