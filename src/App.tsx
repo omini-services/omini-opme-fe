6100bbec67b6b0af156bfc3d489bd16e21e761f9
chameleonor <guilherme.or2013@gmail.com>
2024-02-06 14:14:12 -0300

    trying sentry

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import { CssBaseline, CssVarsProvider } from '@mui/joy';
+import * as Sentry from '@sentry/browser';
 import { RecoilRoot } from 'recoil';
 
 import { Router } from '@/routes';
 
 export function App() {
+  Sentry.init({
+    dsn: 'http://localhost:5173', // Substitua pelo seu DSN
+    // Outras configurações opcionais, como release, environment, etc.
+  });
+
   return (
     <RecoilRoot>
       <CssVarsProvider disableTransitionOnChange>
--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,5 +1,17 @@
+import * as Sentry from '@sentry/browser';
+import { useEffect } from 'react';
+
 import ProfileContent from '@/components/Profile/ProfileContent';
 
-const Dashboard = () => <ProfileContent />;
+const Dashboard = () => {
+  useEffect(() => {
+    try {
+      throw new Error('test');
+    } catch (error) {
+      Sentry.captureException(error);
+    }
+  });
+  return <ProfileContent />;
+};
 
 export default Dashboard;
