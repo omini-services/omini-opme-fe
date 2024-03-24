985ffdb8ddb94802f189ed3d62d9a6273ce140da
chameleonor <guilherme.or2013@gmail.com>
2024-03-23 22:52:39 -0300

    adicionando full calendar

--- src/components/Calendar/index.tsx
+++ src/components/Calendar/index.tsx
@@ -1,49 +1,129 @@
-import Box from '@mui/joy/Box';
-import dayjs from 'dayjs';
-import { Fragment, useState, useEffect } from 'react';
-import { useRecoilValue } from 'recoil';
+import {
+  EventApi,
+  DateSelectArg,
+  EventClickArg,
+  EventContentArg,
+  formatDate,
+} from '@fullcalendar/core';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import interactionPlugin from '@fullcalendar/interaction';
+import FullCalendar from '@fullcalendar/react';
+import timeGridPlugin from '@fullcalendar/timegrid';
+import esLocale from '@fullcalendar/core/locales/pt-br';
+import React, { useState } from 'react';
 
-import { monthIndexState } from '@/atoms/calendar';
+import { INITIAL_EVENTS, createEventId } from './utils';
 
-import Day from './Day';
+import './index.css';
 
-export const getMonth = (month = dayjs().month()) => {
-  const year = dayjs().year();
-  let currentMonthCount = -2;
-  const daysMatrix = new Array(5).fill([]).map(() =>
-    new Array(7).fill(null).map(() => {
-      currentMonthCount += 1;
-      return dayjs(new Date(year, month, currentMonthCount));
-    }),
+const renderEventContent = (eventContent: EventContentArg) => (
+  <>
+    <b>{eventContent.timeText}</b>
+    <i>{eventContent.event.title}</i>
+  </>
+);
+
+const renderSidebarEvent = (event: EventApi) => (
+  <li key={event.id}>
+    <b>
+      {formatDate(event.start!, {
+        year: 'numeric',
+        month: 'short',
+        day: 'numeric',
+      })}
+    </b>
+    <span>
+      <b>
+        {formatDate(event.start!, {
+          hour: 'numeric',
+          minute: 'numeric',
+        })}
+      </b>
+    </span>
+    <span> - </span>
+    <i>{event.title}</i>
+  </li>
+);
+
+const Calendar = () => {
+  const [weekendsVisible, setWeekendsVisible] = useState(true);
+  const [currentEvents, setCurrentEvents] = useState<EventApi[]>([]);
+
+  const renderSidebar = () => (
+    <div className="calendar-sidebar">
+      <div className="calendar-sidebar-section">
+        <h2>Todos os eventos ({currentEvents.length})</h2>
+        <ul>{currentEvents.map(renderSidebarEvent)}</ul>
+      </div>
+      <div className="calendar-sidebar-section">
+        <label>
+          <input
+            type="checkbox"
+            checked={weekendsVisible}
+            onChange={() => setWeekendsVisible(!weekendsVisible)}
+          />
+          Mostrar finais de semana
+        </label>
+      </div>
+    </div>
   );
-  return daysMatrix;
-};
 
-export default function Month() {
-  const monthIndex = useRecoilValue(monthIndexState);
-  const [currenMonth, setCurrentMonth] = useState(getMonth());
+  const handleDateSelect = (selectInfo: DateSelectArg) => {
+    const title = prompt('Please enter a new title for your event');
+    const calendarApi = selectInfo.view.calendar;
 
-  useEffect(() => {
-    setCurrentMonth(getMonth(monthIndex));
-  }, [monthIndex]);
+    calendarApi.unselect(); // clear date selection
+
+    if (title) {
+      calendarApi.addEvent({
+        id: createEventId(),
+        title,
+        start: selectInfo.startStr,
+        end: selectInfo.endStr,
+        allDay: selectInfo.allDay,
+      });
+    }
+  };
+
+  const handleEventClick = (clickInfo: EventClickArg) => {
+    console.log(
+      `Are you sure you want to delete the event '${clickInfo.event.title}'`,
+    );
+    clickInfo.event.remove();
+  };
+
+  const handleEvents = (events: EventApi[]) => {
+    setCurrentEvents(events);
+  };
 
   return (
-    <Box
-      className="month-grid"
-      sx={{
-        width: '1024px',
-        display: 'flex',
-        flexDirection: 'row',
-        flexWrap: 'wrap',
-      }}
-    >
-      {currenMonth.map((row, i) => (
-        <Fragment key={i}>
-          {row.map((day, idx) => (
-            <Day day={day} key={idx} rowIdx={i} />
-          ))}
-        </Fragment>
-      ))}
-    </Box>
+    <div className="calendar">
+      {renderSidebar()}
+      <div className="calendar-main">
+        <FullCalendar
+          height="100%"
+          plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+          headerToolbar={{
+            left: 'prev,next today',
+            center: 'title',
+            right: 'dayGridMonth,timeGridWeek,timeGridDay',
+          }}
+          initialView="dayGridMonth"
+          editable
+          selectable
+          selectMirror
+          dayMaxEvents
+          weekends={weekendsVisible}
+          initialEvents={INITIAL_EVENTS}
+          select={handleDateSelect}
+          eventContent={renderEventContent}
+          eventClick={handleEventClick}
+          eventsSet={handleEvents}
+          locale={esLocale}
+        />
+      </div>
+    </div>
   );
-}
+};
+
+export default Calendar;
