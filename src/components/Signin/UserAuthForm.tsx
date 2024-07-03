cf977f3c2aa9c27d9ea014ede2cdddf181480ce8
Danilo Kasparian <dan.acceto@gmail.com>
2024-07-03 19:48:43 -0400

    add login as first page

--- src/components/Signin/UserAuthForm.tsx
+++ src/components/Signin/UserAuthForm.tsx
@@ -1,35 +1,45 @@
-import { Icons } from '@/components/shadcn/default/icons';
-import { Button } from '@/components/shadcn/new-york/button';
-import { cn } from '@/lib/utils';
 import { useAuth0 } from '@auth0/auth0-react';
-import { useState } from 'react';
+import { useEffect } from 'react';
 
-interface UserAuthFormProps extends React.HTMLAttributes<HTMLDivElement> {}
+interface UserAuthFormProps extends React.HTMLAttributes<HTMLDivElement> { }
 
 export function UserAuthForm({ className, ...props }: UserAuthFormProps) {
-  const [isLoading, setIsLoading] = useState<boolean>(false);
-  const { loginWithRedirect } = useAuth0();
+  // const [isLoading, setIsLoading] = useState<boolean>(false);
+  const { loginWithRedirect, error } = useAuth0();
 
-  const handleLogin = () => {
+  useEffect(() => {
+    // Perform login when component mounts
     loginWithRedirect().catch((e) => {
-      console.log(e);
+      console.error('Authentication error:', e);
     });
-  };
+  }, [loginWithRedirect]);
 
-  const onSubmit = (event: React.SyntheticEvent) => handleLogin();
+  // const handleLogin = () => {
+  //   loginWithRedirect().catch((e) => {
+  //     console.log(e);
+  //   });
+  // };
 
-  return (
-    <div className={cn('grid gap-6', className)} {...props}>
-      <form onSubmit={onSubmit}>
-        <div className="grid gap-2">
-          <Button disabled={isLoading}>
-            {isLoading && (
-              <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
-            )}
-            Entrar
-          </Button>
-        </div>
-      </form>
-    </div>
-  );
+  // const onSubmit = (event: React.SyntheticEvent) => handleLogin();
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
+  // return (
+  //   <div>
+  //     <Loading />
+  //   </div>
+    // <div className={cn('grid gap-6', className)} {...props}>
+    //   <form onSubmit={onSubmit}>
+    //     <div className="grid gap-2">
+    //       <Button disabled={isLoading}>
+    //         {isLoading && (
+    //           <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
+    //         )}
+    //         Entrar
+    //       </Button>
+    //     </div>
+    //   </form>
+    // </div>
+  //);
 }
--- src/pages/Signin/index.tsx
+++ src/pages/Signin/index.tsx
@@ -1,26 +1,25 @@
-import { Brand } from '@/components/Signin/Brand';
 import { UserAuthForm } from '@/components/Signin/UserAuthForm';
 
 export default function AuthenticationPage() {
   return (
-    <div
-      id="auth"
-      className="container relative h-full max flex-col items-center justify-center grid md:grid lg:max-w-none lg:grid-cols-2 lg:px-0"
-    >
-      <div className="relative hidden h-full flex-col bg-muted p-10 text-white lg:flex dark:border-r">
-        <div className="absolute inset-0 bg-zinc-900" />
-        <div className="relative z-20 h-full flex items-center justify-center text-lg font-medium">
-          <Brand className="w-96" />
-        </div>
-      </div>
-      <div className="lg:p-8">
-        <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
-          <div className="flex flex-col space-y-2 text-center">
-            <h1 className="text-2xl font-semibold tracking-tight">Bem vindo</h1>
-          </div>
+    // <div
+    //   id="auth"
+    //   className="container relative h-full max flex-col items-center justify-center grid md:grid lg:max-w-none lg:grid-cols-2 lg:px-0"
+    // >
+    //   <div className="relative hidden h-full flex-col bg-muted p-10 text-white lg:flex dark:border-r">
+    //     <div className="absolute inset-0 bg-zinc-900" />
+    //     <div className="relative z-20 h-full flex items-center justify-center text-lg font-medium">
+    //       <Brand className="w-96" />
+    //     </div>
+    //   </div>
+    //   <div className="lg:p-8">
+    //     <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
+    //       <div className="flex flex-col space-y-2 text-center">
+    //         <h1 className="text-2xl font-semibold tracking-tight">Bem vindo</h1>
+    //       </div>
           <UserAuthForm />
-        </div>
-      </div>
-    </div>
+    //     </div>
+    //   </div>
+    // </div>
   );
 }
